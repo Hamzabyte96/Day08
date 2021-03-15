7675e6afdc6701802a696fcdcc632b46d950da02
Hamzabyte96 <brcaninovichamza@yahoo.com>
2021-03-15 23:37:43 +0100

    izmjene ex12

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function mathTest(x, y) {
+  if (x < 0 || y < 0) {
+    return undefined;
+  
+    
+  }
+}
+
+Math.round(Math.pow(Math.sqrt(2) + Math.sqrt(2), 2));
+    console.log(Math.round(Math.pow(Math.sqrt(-2) + Math.sqrt(2), 2)))
+    console.log(Math.pow(Math.sqrt(2) + Math.sqrt(-2), 2));
+    return Math.round(Math.pow(Math.sqrt(2) + Math.sqrt(8), 2));
+    return Math.round(Math.pow(Math.sqrt(3) + Math.sqrt(3), 2));
+    return Math.round(Math.pow(Math.sqrt(0) + Math.sqrt(0), 2));
+module.exports = mathTest;
---
+++ ex14/src/index.js
@@ -0,0 +1,8 @@
+function convertStrToInt(str) {
+    var str = parseInt("44");
+    console.log(str);
+    
+}
+
+convertStrToInt(str);
+module.exports = convertStrToInt;
